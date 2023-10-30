15320831483c4a9bf9e1556e8466505c4646186c
kareemassad <kareemassad5@gmail.com>
2023-10-30 06:37:47 -0400

    Finished Part 2 of lab6 for sysc4806

--- Classes/SYSC4806/Labs/Lab6/src/main/resources/static/js/spa.js
+++ Classes/SYSC4806/Labs/Lab6/src/main/resources/static/js/spa.js
@@ -1,5 +1,9 @@
 // spa.js
 function addBuddy(addressBookId, name, phoneNumber) {
+    if (!addressBookId) {
+        console.error('addressBookId is null or undefined');
+        return;
+    }
     const buddyInfo = { name: name, phoneNumber: phoneNumber };
     fetch(`/api/addressBooks/${addressBookId}/addBuddy`, {
         method: 'POST',
@@ -16,9 +20,17 @@ function addBuddy(addressBookId, name, phoneNumber) {
 }
 
 function refreshBuddyList(addressBookId) {
+    if (!addressBookId) {
+        console.error('addressBookId is null or undefined');
+        return;
+    }
     fetch(`/api/addressBooks/${addressBookId}/view`)
         .then(response => response.json())
         .then(data => {
+            if (!data || !data.buddies) {
+                console.error('Unexpected data received:', data);
+                return;
+            }
             const tableBody = document.getElementById('buddy-table-body');
             tableBody.innerHTML = '';
             data.buddies.forEach(buddy => {
@@ -33,7 +45,9 @@ function refreshBuddyList(addressBookId) {
 
 function initialize() {
     const addressBookId = document.body.getAttribute('data-address-book-id');
-    document.getElementById('add-buddy-button').addEventListener('click', function(event) {
+    const addButton = document.getElementById('add-buddy-button');
+    console.log(addButton);  // This should log the element, or 'null' if it's not found
+    addButton.addEventListener('click', function(event) {
         event.preventDefault();
         const name = document.getElementById('buddy-name').value;
         const phone = document.getElementById('buddy-phone').value;
@@ -42,3 +56,40 @@ function initialize() {
 }
 
 window.onload = initialize;
+
+// spa.js
+let addressBookId = null;
+
+document.getElementById('create-address-book-button').addEventListener('click', function() {
+    fetch('/api/createAddressBook', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            addressBookId = data.id;
+            document.getElementById('create-address-book-section').style.display = 'none';
+            document.getElementById('add-buddy-section').style.display = 'block';
+            document.getElementById('buddies-table').style.display = 'block';
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+document.getElementById('add-buddy-button').addEventListener('click', function() {
+    const name = document.getElementById('buddy-name').value;
+    const phoneNumber = document.getElementById('buddy-phone').value;
+    const buddyInfo = { name: name, phoneNumber: phoneNumber };
+    fetch(`/api/addressBooks/${addressBookId}/addBuddy`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(buddyInfo)
+    })
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.getElementById('buddy-table-body');
+            const row = tableBody.insertRow();
+            row.insertCell(0).innerText = data.id;
+            row.insertCell(1).innerText = data.name;
+            row.insertCell(2).innerText = data.phoneNumber;
+        })
+        .catch(error => console.error('Error:', error));
+});
